d8a81a861e7fb4f7bee4b39ce5b26c835d9055e7
Mike Ulvila <mikeulvila@me.com>
2015-11-06 10:28:57 -0600

    add music working with handlebars

--- javascripts/addmusic.js
+++ javascripts/addmusic.js
@@ -1,19 +1,36 @@
 define(
 	["jquery", "script"],
 	function($, script) {
-		var titles = [],
-			artists = [],
-			albums = [];
-			console.log(script);
+		//******OLD VARIABLES*********
+		// var titles = [],
+		// 	artists = [],
+		// 	albums = [];
+			// console.log(script);
 		$("#add").click(function() {
 			console.log("add button works");
-			titles.push($("#song-title").val());
-			artists.push($("#song-artist").val());
-			albums.push($("#song-album").val());
-			console.log(titles);
-			console.log(artists);
-			console.log(albums);
-			script.mainWindow.append("<div id='song-container'><h2>" + titles[titles.length - 1] + "</h2><ul class='song-info'><li>" + artists[artists.length - 1] + " </li><li class='song-info-border'>" + albums[albums.length - 1] + "</li></ul><button class='delete-button'>Delete</button></div>");
+			var inputSong = {
+				songs: [
+					{
+						title: $("#song-title").val(),
+						artist: $("#song-artist").val(),
+						album: $("#song-album").val()
+					}
+				]
+			};
+			
+			console.log($("#song-title").val());
+			console.log("song", inputSong);
+			require(['hbs!../templates/songs'], function(songTemplate) {
+				script.mainWindow.append(songTemplate(inputSong));
+			});
+			//***********OLD CODE*******************
+			// titles.push($("#song-title").val());
+			// artists.push($("#song-artist").val());
+			// albums.push($("#song-album").val());
+			// console.log(titles);
+			// console.log(artists);
+			// console.log(albums);
+			// script.mainWindow.append("<div id='song-container'><h2>" + titles[titles.length - 1] + "</h2><ul class='song-info'><li>" + artists[artists.length - 1] + " </li><li class='song-info-border'>" + albums[albums.length - 1] + "</li></ul><button class='delete-button'>Delete</button></div>");
 			$("#main-window").show();
 			$("#side-nav").show();
 			$("#enter-song-info").hide();
