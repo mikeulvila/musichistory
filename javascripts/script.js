1c557190f2d4abf7b83ef96a22de35777a6f02e5
Mike Ulvila <mikeulvila@me.com>
2015-11-10 11:46:38 -0600

    got filter module working

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,10 +4,11 @@ define(
 	function($, hbs, populate, getMore) {
 		
 		$("#enter-song-info").hide();
-		
+	//**********DECLARE VARIABLES TO RETURN AT END OF PAGE**********
 		var mainWindow = $("#song-list-container");
 		var artistSelect = $("#artist-select");
 		var albumSelect = $("#album-select");
+	//********** USE HANDLEBARS TEMPLATES TO POPULATE DOM WITH INFO FROM JSON************
 		var getSongInfo = function (songInfo) {
 			require(['hbs!../templates/songs', 'hbs!../templates/artist_select', 'hbs!../templates/album_select'], 
 				function(songTemplate, artistSelectTemplate, albumSelectTemplate) {
@@ -16,24 +17,10 @@ define(
 				albumSelect.html(albumSelectTemplate(songInfo));
 
 			});
-			// for (var i = 0; i < songs.length; i++) {
-		 //    	mainWindow.append("<div id='song-container'><h2>" + songs[i].title + "</h2><ul class='song-info'><li>" + songs[i].artist + " </li><li class='song-info-border'>" + songs[i].album + "</li></ul><button class='delete-button'>Delete</button></div>");
-		 //    }
 		};
-
-
+	//************CALL JSON REQUEST AND INSERT getSongInfo as CALLBACK**********
 		populate.getJsonData(getSongInfo);
-		// require(['hbs!../templates/songs'], function(songTemplate) {
-		// 	$("#main-window").html(songTemplate(songs));
-		// 	})
-	
 
-
-	// $.ajax({url: "data/songs.json"}).done(getSongInfo); 
-//*****************ADD MORE SONGS WITH MORE BUTTON***************************
-	// $("#more-button").click(function(){
-	// 	getMore.getJsonData(getSongInfo);
-	// });
 //***************DISPLAY AND HIDE LIST MUSIC AND ADD MUSIC********************
 	$("#list-music").click(function(){
 		$("#main-window").show();
@@ -49,34 +36,12 @@ define(
 	console.log("add music works");
 	});
 
-//*************DECLARE VARIABLES******************************
-// var titles = [],
-// 	artists = [],
-// 	albums = [];
-
-//******************ADD MUSIC************************
-	// $("#add").click(function() {
-	// 	console.log("add button works");
-	// 	titles.push($("#song-title").val());
-	// 	artists.push($("#song-artist").val());
-	// 	albums.push($("#song-album").val());
-	// 	console.log(titles);
-	// 	console.log(artists);
-	// 	console.log(albums);
-	// 	mainWindow.append("<div id='song-container'><h2>" + titles[titles.length - 1] + "</h2><ul class='song-info'><li>" + artists[artists.length - 1] + " </li><li class='song-info-border'>" + albums[albums.length - 1] + "</li></ul><button class='delete-button'>Delete</button></div>");
-	// 	$("#main-window").show();
-	// 	$("#side-nav").show();
-	// 	$("#enter-song-info").hide();
-	// 	$("#song-title").val("");
-	// 	$("#song-artist").val("");
-	// 	$("#song-album").val("");
-	// });
-
 /****************DELETE ROW OF SONGS*********************/
 	$(document).on("click", ".delete-button", function(event){
 		console.log("delete button works", event);	
 		event.target.parentElement.remove();
 	});
+
 	//*********RETURN DOM VARIABLES
 	return {
 		mainWindow: mainWindow,
@@ -84,6 +49,7 @@ define(
 		albumSelect: albumSelect,
 		getSongInfo: getSongInfo
 	};
+
 
 });
 
