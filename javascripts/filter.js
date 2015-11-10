92b973e33e2e50f48bc1ceb811f3a2f1c3a404a8
Mike Ulvila <mikeulvila@me.com>
2015-11-10 15:05:07 -0600

    fixed delete

---
+++ javascripts/filter.js
@@ -0,0 +1,32 @@
+define(
+	["jquery"],
+	function($) {
+	// *************FILTER ARTISTS***********************
+	$("#artist-select").change(function(e) {
+		var filteredArtist = $("#artist-select option:selected").text();
+		// console.log("selected artist", filteredArtist);
+		$(".song-container").show();
+		$(".song-artist").each(function() {
+			// console.log("THIS", $(this).text().length, filteredArtist.length);
+			if ($(this).text() === filteredArtist) {
+				$(this).parents(".song-container").show();
+			} else {
+				console.log("hiding");
+				$(this).parents(".song-container").hide();
+			}
+		});
+	});
+
+	//************* FILTER ALBUMS ******************
+	$("#album-select").change(function(e) {
+		var filteredAlbum = $("#album-select option:selected").text();
+		console.log("selected album", filteredAlbum);
+		$(".song-container").show();
+		$(".song-container").not(':contains("' + filteredAlbum + '")').hide();
+	});
+
+	//************* CLEAR FILTER *******************
+	$("#clear-filter").click(function() {
+		$(".song-container").show();
+	});
+}); 
